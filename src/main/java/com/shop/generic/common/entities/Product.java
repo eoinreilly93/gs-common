956c28b4734d55d63e89ff305e601753b71950a4
eoinreilly <eoindreilly@gmail.com>
2023-01-22 16:09:05 +0000

    refactor: Use Lombok's Data annotation
    The data annotation by default includes the getters, setters equals and hashcodes, so it is better to use that manually specifying getters/setters manually

--- src/main/java/com/shop/generic/common/entities/Product.java
+++ src/main/java/com/shop/generic/common/entities/Product.java
@@ -9,10 +9,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.math.BigDecimal;
-import lombok.Getter;
+import lombok.Data;
 
 @Entity
-@Getter
+@Data
 public class Product {
 
     @Id
