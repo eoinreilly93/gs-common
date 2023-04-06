adc17085d0370a5c6743693d3c30e323db96142a
eoinreilly <eoindreilly@gmail.com>
2023-04-06 18:27:21 +0100

    feat: Add productId to the VO object
    Required in the purchasing-service in the ProductsCanBePurchasedValidator to identify the quantity of products available to purchase

--- src/main/java/com/shop/generic/common/valueobjects/ProductVO.java
+++ src/main/java/com/shop/generic/common/valueobjects/ProductVO.java
@@ -4,10 +4,12 @@ import com.shop.generic.common.entities.Product;
 import com.shop.generic.common.enums.StockStatus;
 import java.math.BigDecimal;
 
-public record ProductVO(String name, BigDecimal price, StockStatus stockStatus, int stockCount) {
+public record ProductVO(int id, String name, BigDecimal price, StockStatus stockStatus,
+                        int stockCount) {
 
     public ProductVO(final Product product) {
-        this(product.getName(), product.getPrice(), product.getStockStatus(),
+        this(product.getProductId(), product.getName(), product.getPrice(),
+                product.getStockStatus(),
                 product.getStockCount());
     }
 }
