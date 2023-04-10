452b427b4e58b1ddc2a158c00a15d60da4b9394f
eoinreilly <eoindreilly@gmail.com>
2023-04-10 18:11:06 +0100

    feat: Create and persist order to database
    Also includes minor typo fixes and some additional todos

--- src/main/java/com/shop/generic/common/dtos/PurchaseProductDTO.java
+++ src/main/java/com/shop/generic/common/dtos/PurchaseProductDTO.java
@@ -1,11 +1,14 @@
 package com.shop.generic.common.dtos;
 
+import java.math.BigDecimal;
+
 /**
  * A VO representing a request to purchase a product
  *
  * @param productId the ID of the product
  * @param quantity  the quantity of the product to be purchased
+ * @param price     the price of the product
  */
-public record PurchaseProductDTO(int productId, int quantity) {
+public record PurchaseProductDTO(int productId, int quantity, BigDecimal price) {
 
 }
