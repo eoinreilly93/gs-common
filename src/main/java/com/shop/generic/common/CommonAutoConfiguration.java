82f058a3fe66b44d0ea0530bd2397117d514d58d
eoinreilly <eoindreilly@gmail.com>
2024-09-26 23:46:27 +0100

    feat: Add autoconfiguration for the common project
    - Configure autoconfiguration on the common project so that all the beans will be autowired when the downstream applications start up. Removes the need for the other apps to include a package scan, as this will be defined in the autoconfiguration.
    
    - @EntityScan is still required because Spring Boot processes JPA-related configurations (like @EntityScan) in the main application context, which is where the persistence context is set up.

---
+++ src/main/java/com/shop/generic/common/CommonAutoConfiguration.java
@@ -0,0 +1,10 @@
+package com.shop.generic.common;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.shop.generic.common")
+public class CommonAutoConfiguration {
+
+}
