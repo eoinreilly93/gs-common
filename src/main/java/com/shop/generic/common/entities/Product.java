04f56af6a72798e04f57b454e466ba74daace6ad
eoinreilly <eoindreilly@gmail.com>
2024-10-07 21:15:30 +0100

    refactor: Define products table name instead of letting it be inferred

--- src/main/java/com/shop/generic/common/entities/Product.java
+++ src/main/java/com/shop/generic/common/entities/Product.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +18,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@Table(name = "products")
 public class Product {
 
     @Id
